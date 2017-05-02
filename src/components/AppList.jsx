415882720484ce366fe10fce27774f4d464e1611
Rishabh Parekh <rjain15@gmail.com>
2017-05-02 16:55:26 -0400

    Commit AppList changes

--- src/components/AppList.jsx
+++ src/components/AppList.jsx
@@ -4,10 +4,15 @@ import {TileLayout} from 'pui-react-tile-layout'
 import axios from 'axios';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Input  from 'pui-react-inputs';
 
 class AppList extends Component {
   componentWillMount() {
     this.props.fetchApps();
+    this.state = {
+      filter: "",
+      appList: this.props.appList
+    };
   }
 
   renderApp(app) {
@@ -17,10 +22,12 @@ class AppList extends Component {
   }
 
   render() {
-    const { appList } = this.props;
+    const filterRegex = new RegExp(this.state.filter, "i");
+    const appList = this.state.appList.map(app => app.match(filterRegex) && <li key={app}>{app}</li>);
     return (
       <div>
         <h2>App List</h2>
+        <Input search placeholder="Filter by..." onChange={event => this.setState({ filter: event.target.value })}/>
         <TileLayout columns={{xs: 1, sm: 2, md: 3}}>
           {appList.map(this.renderApp)}
         </TileLayout>
