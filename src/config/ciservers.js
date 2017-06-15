4ad066d2d982a36d0d52b792cbb4500d28ec7847
Rajesh Jain <rjain15@gmail.com>
2017-06-15 15:49:02 -0700

    Added the new ciservers

--- src/config/ciservers.js
+++ src/config/ciservers.js
@@ -1,13 +1,18 @@
 const ciservers = [
   {
-    "name": "AWS",
+    "name": "Prod",
     "api": "https://concourse-c0.gcp.rjainpcf.com",
     "team": "ford"
   },
   {
-    "name": "Brian's",
-    "api": "https://concourse-c0.gcp.bbyerspcf.com",
-    "team": "test"
+    "name": "Team-A",
+    "api": "https://concourse-c0.gcp.rjainpcf.com",
+    "team": "team-a"
+  },
+  {
+    "name": "Team-B",
+    "api": "https://concourse-c0.gcp.rjainpcf.com",
+    "team": "team-b"
   }
 ]
 
